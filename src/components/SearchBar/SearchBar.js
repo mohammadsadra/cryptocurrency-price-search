2dfba4db5ee9ce5142dd1861f55d05d76f4f2016
Mohammad Sadra Haeri <43936083+mohammadsadra@users.noreply.github.com>
2023-12-26 18:18:11 +0330

    Added searchbar.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './SearchBar.css'; // Import the CSS file for styles
 
 function SearchBar({ searchTerm ,setSearchTerm }) {
+    const searchRef = useRef(null);
+    useEffect(() => {
+        searchRef.current.classList.add('visible');
+    }, []);
     return (
-        <div className="search-bar">
+        <div className="search-bar search" ref={searchRef}>
             <input
                 style={{fontSize: "1.5rem"}}
                 type="text"
